5ed644ffb4916fe0b456a175b6cf46d41a692154
Priya X. Pramesi <ppramesi@gmail.com>
2021-07-16 12:35:30 +0700

    fixed user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -21,6 +21,9 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'full_name',
+        'purchase_history',
+        'read_history'
     ];
 
     /**
